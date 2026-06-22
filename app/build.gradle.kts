d50ce70508ddc2a3d0bdbd1a38717fe8ab2a8803
eigger <eigger87@gmail.com>
2026-06-22 21:56:40 +0900

    Update versionCode and versionName in build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "dev.eigger.hassble"
         minSdk = 26
         targetSdk = 35
-        versionCode = 10
-        versionName = "0.1.9"
+        versionCode = 11
+        versionName = "0.1.10"
     }
 
     buildTypes {
