104e7e60a0ffa7ef30d914ba41bb2bb6e916300e
eigger <eigger87@gmail.com>
2026-06-23 20:32:24 +0900

    Fix entity cleanup bug on config change and bump version to 0.2.6 (#25)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "dev.eigger.hassble"
         minSdk = 26
         targetSdk = 35
-        versionCode = 24
-        versionName = "0.2.5"
+        versionCode = 25
+        versionName = "0.2.6"
     }
 
     signingConfigs {
