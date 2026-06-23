e0447aeb187b24b0e7f414fdb0a30d1c74eb01e4
eigger <eigger87@gmail.com>
2026-06-23 19:57:58 +0900

    Fix beacon scanning: Remove neverForLocation and request location permission on Android 12+ (#23)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "dev.eigger.hassble"
         minSdk = 26
         targetSdk = 35
-        versionCode = 22
-        versionName = "0.2.3"
+        versionCode = 23
+        versionName = "0.2.4"
     }
 
     signingConfigs {
