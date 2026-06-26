402dc69beb2a9ffdde0d3b77c04c175bf3f974ef
eigger <eigger87@gmail.com>
2026-06-27 00:03:15 +0900

    Bump version code to 52 and update version name to 1.0.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "dev.eigger.hassble"
         minSdk = 26
         targetSdk = 35
-        versionCode = 51
-        versionName = "0.3.22"
+        versionCode = 52
+        versionName = "1.0.0"
     }
 
     signingConfigs {
