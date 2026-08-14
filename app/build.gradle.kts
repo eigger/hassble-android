aafa8a245cc85697f1f3d92394de7629a8daf0fe
eigger <eigger87@gmail.com>
2026-08-14 17:04:31 +0900

    Update versionCode and versionName to 64 and 1.2.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "dev.eigger.hassble"
         minSdk = 26
         targetSdk = 35
-        versionCode = 63
-        versionName = "1.1.1"
+        versionCode = 64
+        versionName = "1.2.0"
     }
 
     signingConfigs {
